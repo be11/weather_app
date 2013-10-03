a6c8470a37bf168b95825c6853c65caa7fc8866c
be11 <o.o.8.o.o.bell@gmail.com>
2013-10-03 14:20:59 +0900

    Delete forecast_today.rb

--- app/models/forecast_today.rb
+++
@@ -1,3 +0,0 @@
-class ForecastToday < ActiveRecord::Base
-  attr_accessible :clouds, :description, :humidity, :icon, :main, :pref_id, :pressure, :rain, :snow, :temp, :temp_max, :temp_min, :time, :winddeg, :windspeed
-end
